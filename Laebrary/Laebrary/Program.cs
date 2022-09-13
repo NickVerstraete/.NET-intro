7676cc29beead8094d87d9c3936e4b2bf378d855
Nick Verstraete <nick.verstraete@fluxys.com>
2022-09-13 17:02:39 +0200

    add support for mysql

--- Laebrary/Laebrary/Program.cs
+++ Laebrary/Laebrary/Program.cs
@@ -1,14 +1,14 @@
 using Laebrary.Models;
 using Microsoft.EntityFrameworkCore;
 
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<LaebraryContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("LaebraryDb")));
-       
-//Add Repositories
+//builder.Services.AddDbContext<LaebraryContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServerLaebraryDb")));
+builder.Services.AddDbContext<LaebraryContext>(options => options.UseMySql(builder.Configuration.GetConnectionString("MySqlLaebraryDb"), ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("MySqlLaebraryDb"))));
 
+//Add Repositories
+ 
 
 //Add Services
 
